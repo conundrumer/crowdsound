b937a215462f9be61c97c1c695176ebf3a844f4d
David Lu <conundrumer@gmail.com>
2014-03-21 01:50:48 -0400

    getting comments from soundcloud

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ define( ['scwrapper','polytts'], function (SC, Polytts) {
 
 	var App = {
 		init: function() {
-		SC.init();
-		Polytts.init();
-		Polytts.hello();
+			SC.init();
+			Polytts.init();
+			Polytts.hello();
+			SC.setTrack('https://soundcloud.com/herobust/sheknowshebad', function() {
+				console.log(SC.comments.length);
+				console.log(SC.comments[0]);
+			});
 		}
 	};
 
--- js/scwrapper.js
+++ js/scwrapper.js
@@ -3,16 +3,57 @@
  */
 define( ['soundcloud'], function (SC) {
 
-    var MY_CLIENT_ID = 'f4352d8c413a0a2ab164d974da1c9083';
+    var MY_CLIENT_ID = 'f4352d8c413a0a2ab164d974da1c9083',
+    PAGE_SIZE = 200,
+    MAX_OFFSET = 8000;
 
 	var SCwrapper = {
+        trackID: null,
+        trackUrl: null,
+
         init: function () {
             SC.initialize({
                 client_id: MY_CLIENT_ID
             });
         },
-        getComments: function (url) {
-            console.error("Not yet implemented");
+
+        setTrack: function (trackUrl, callback) {
+            if (this.trackUrl === trackUrl) return;
+            this.trackUrl = trackUrl;
+            this.comments = []; // reset comments
+            callback = callback || function() {};
+            var self = this;
+            SC.get('/resolve', { url: trackUrl },
+                function (trackData) {
+                    self.trackID = trackData.id;
+                    self.setComments(0, callback);
+                });
+        },
+
+        setComments: function (offset, callback) {
+            var self = this;
+            SC.get('/tracks/' + self.trackID + '/comments',
+                { limit: PAGE_SIZE, offset: offset},
+                function (comments) {
+                    self.comments = self.comments.concat(self.formatComments(comments));
+                    if (comments.length === PAGE_SIZE && offset < MAX_OFFSET) {
+                        self.setComments(offset + PAGE_SIZE, callback);
+                    } else {
+                        callback();
+                    }
+                });
+        },
+
+        formatComments: function (comments) {
+            return comments.map(function (comment) {
+                return {
+                    body: comment.body,
+                    timestamp: comment.timestamp,
+                    user: comment.user.username
+                };
+            }).filter(function (comment) {
+                return comment.timestamp !== null;
+            });
         }
     };
 
